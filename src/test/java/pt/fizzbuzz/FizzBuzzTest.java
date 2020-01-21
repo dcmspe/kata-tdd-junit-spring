2d6901c11083d03b5121db52128dd7e9ab0c077a
Danilo Caetano Matias dos Santos <danilocaetano.santos@externe.bnpparibas.com>
2020-01-21 14:45:36 +0000

    Fixing the tests.

--- src/test/java/pt/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/pt/fizzbuzz/FizzBuzzTest.java
@@ -2,7 +2,9 @@ package pt.fizzbuzz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class FizzBuzzTest {
 	
 	  @Test
